6cad0bb88e770380de657e099974b698573bafc4
Samuelato <edithatoneche@gmail.com>
2025-03-03 18:08:33 +0100

    Add files via upload

--- homework class robot/rect.py
+++ homework class robot/rect.py
@@ -14,7 +14,14 @@ class Rectangle:
     def draw(self):
         pygame.draw.rect(screen,self.color,(self.x,self.y,self.height,self.width))
 
-rect1=Rectangle("black",70,30,300,200)
+rect1=Rectangle("black",35,35,35,0)
+rect2=Rectangle("white",35,35,0,0)
+rect3=Rectangle("white",35,35,70,0)
+rect4=Rectangle("black",35,35,105,0)
+rect5=Rectangle("white",35,35,140,0)
+rect6=Rectangle("black",35,35,0,35)
+rect7=Rectangle("white",35,35,35,35)
+rect8=Rectangle("black",35,35,70,35)
 
 #game loop
 running=True
@@ -24,6 +31,14 @@ while running:
             running=False
     screen.fill("green") 
     rect1.draw()
+    rect2.draw()
+    rect3.draw()
+    rect4.draw()
+    rect5.draw()
+    rect6.draw()
+    rect7.draw()
+    rect8.draw()
+    DeprecationWarning
     pygame.display.update()
 
 pygame.quit()
